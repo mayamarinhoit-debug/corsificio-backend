9b6007c8173672a3ad86f19f5970d4d55810ea3d
mayamarinhoit-debug <mayamarinhoit@gmail.com>
2026-08-22 16:39:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -817,8 +817,8 @@ app.post('/crea-abbonamento', async (req, res) => {
       mode: 'subscription',
       line_items: [{ price: priceId, quantity: 1 }],
       customer_email: user.email || undefined,
-      success_url: `https://www.google.com/?sub=1&session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `https://www.google.com/?sub=0`,
+      success_url: `https://www.google.com/`,
+      cancel_url: `https://www.google.com/`,
       metadata: { user_id: user.id },
     });
 
